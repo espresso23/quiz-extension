a74cc03600d5673ca9c315c89c04e1447504a8b5
TanPLQ <phamlequoctan@gmail.com>
2026-04-14 19:36:10 +0700

    Fix: remove sidePanel/activeTab/scripting permissions causing content script failure

---
+++ test.js
@@ -0,0 +1,4 @@
+// Simple test script
+console.log('=== TEST EXTENSION LOADED ===');
+console.log('If you see this, content scripts ARE working.');
+alert('Test extension loaded! Content scripts work.');
